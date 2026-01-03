03a9f8cfa120cc6c86bedd7bf452639118bed72d
Prajjwal Vijaywargiya <prajjwal1908@gmail.com>
2026-01-03 14:17:02 +0530

    Use updaters for dot and add label

--- src/scenes/DotTracker.py
+++ src/scenes/DotTracker.py
@@ -7,8 +7,14 @@ class DotTracker(Scene):
         def dot_pos():
             return RIGHT*x.get_value()
         
-        dot = always_redraw(
-            lambda: Dot().move_to(dot_pos())
+        dot = Dot()
+
+        dot.add_updater(
+            lambda m: m.move_to(dot_pos())
+        )
+
+        label = always_redraw(
+            lambda: Text(f"x = {x.get_value():.1f}", font="Arial").next_to(dot, UP)
         )
 
         arrow = always_redraw(
@@ -17,6 +23,7 @@ class DotTracker(Scene):
             )
         )
 
-        self.add(dot, arrow)
+        self.add(dot, arrow, label)
         self.play(x.animate.set_value(3))
+        dot.clear_updaters()
         self.wait()
